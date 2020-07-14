8b9d0ee071eabbf79f6706bd31c6ced03fe0eec6
abudnikov <abudnikov@gridgain.com>
2020-07-14 16:20:17 +0300

    updates

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -1,6 +1,6 @@
 import './code-tabs.js?1'
 import {hideLeftNav} from './docs-menu.js'
-import {hideTopNav} from './top-navigation.js'
+//import {hideTopNav} from './top-navigation.js'
 import './page-nav.js'
 
 
